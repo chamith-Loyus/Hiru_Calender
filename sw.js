9c941d10c402a0bc4e2da82cc9755f8178d8944a
Chamith_Lotus <chamith@lotustechnologies.lk>
2025-12-19 17:36:20 +0530

    Initial commit - PWA Hiru TV Calendar 2026

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'hiru-calendar-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/logo.png',
+  './icons/logo.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js',
+  'https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js',
+  'https://www.hirutv.lk/assets/images/logo.png',
+  'https://www.hirutv.lk/assets/images/hiru%20fm%20logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
